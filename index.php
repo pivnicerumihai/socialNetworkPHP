f92b02fc058ed7d6e80be2d4c1a7626f2a9f5bef
ML Pivniceru <mcpivniceru@gmail.com>
2020-05-01 20:36:01 +0100

    added infinite scrolling for posts

--- index.php
+++ index.php
@@ -35,10 +35,70 @@ if(isset($_POST['post'])){
                     <input type="submit" name="post" id="post" value="Post">
                     <hr>
                 </form>
-<?php
-              $post = new Post($con, $userLoggedIn);
-              $post -> loadPostsFriends();
-     ?>    </div>
+
+            <div class="posts_area"></div>
+            <img id="loading" src="assets/images/icons/loading.gif">
+                </div>
+     <script>
+     
+     $(function(){
+ 
+ var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+ var inProgress = false;
+
+ loadPosts(); 
+
+ $(window).scroll(function() {
+     var bottomElement = $(".status_post").last();
+     var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+    if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+         loadPosts();
+     }
+ });
+
+ function loadPosts() {
+     if(inProgress) { 
+         return;
+     }
+     
+     inProgress = true;
+     $('#loading').show();
+
+     var page = $('.posts_area').find('.nextPage').val() || 1;
+     $.ajax({
+         url: "includes/handlers/ajax_load_posts.php",
+         type: "POST",
+         data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+         cache:false,
+
+         success: function(response) {
+             $('.posts_area').find('.nextPage').remove(); 
+             $('.posts_area').find('.noMorePosts').remove(); 
+             $('.posts_area').find('.noMorePostsText').remove();
+
+             $('#loading').hide();
+             $(".posts_area").append(response);
+
+             inProgress = false;
+         }
+     });
+ }
+
+ //Check if the element is in view
+ function isElementInView (el) {
+     var rect = el.getBoundingClientRect();
+
+     return (
+         rect.top >= 0 &&
+         rect.left >= 0 &&
+         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
+         rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+     );
+ }
+});
+
+     </script>
 </div>
 </body>    
 </html>
